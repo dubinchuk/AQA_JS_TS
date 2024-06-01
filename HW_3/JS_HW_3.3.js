40237d4102a5981303b5e9a4483f0d11748e6eb3
dubinchuk <dubinchuk.e@gmail.com>
2024-06-01 15:07:48 +0300

    Rename JS_HW_1 to HW_3

---
+++ HW_3/JS_HW_3.3.js
@@ -0,0 +1,15 @@
+// Task3**. Напишите программу, которая принимает целое положительное число n (одно любое число от 1 до 9), 
+// и выводит сумму равную n + nn + nnn, где n не перемножаются, а конкатенируются
+
+const nSum = function(n){
+    if (n >= 1 && n <= 9 && n % 1 === 0) {
+        const nString = String(n);
+        const result = Number(nString) + Number(nString + nString) + Number(nString + nString + nString);
+        alert('Сумма n + nn + nnn = ' + result);
+    } else {
+        alert('Введите верное значение');
+    }
+}
+
+let nPrompt = prompt('Введите целое число от 1 до 9');
+nSum(nPrompt);
