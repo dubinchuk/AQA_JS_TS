61cf19872c6ac247946d58aa0ea3375f6c3bb2f6
dubinchuk <dubinchuk.e@gmail.com>
2024-06-03 16:13:53 +0300

    HW_5 added for review

---
+++ HW_5/task 5.2.4.js
@@ -0,0 +1,17 @@
+// 4. Написать скрипт, который преобразует любое предложение в camelCase. Первое слово должно начинаться с буквы в нижнем регистре, 
+//   у остальных -  верхнем. Пример: I am super engineer => iAmSuperEngineer
+
+function toCamelCase (sentence) {
+    const splitSentence = sentence.toLowerCase().split(' ');
+    let camelCaseSentence = splitSentence[0];
+    for (let i = 1; i < splitSentence.length; i++) {
+        const splitWord = splitSentence[i].split('');
+        splitWord[0] = splitWord[0].toUpperCase();;
+        const camelCaseWord = splitWord.join('');
+        camelCaseSentence += camelCaseWord;
+        if (i == splitSentence.length - 1) {
+            console.log(camelCaseSentence);
+        }
+    }
+}
+toCamelCase('I am super engineer');
---
+++ HW_5/task 5.3.1.js
@@ -0,0 +1,24 @@
+// Task 3
+// 1*. Создать программу, которая будет принимать на вход СЛОВО (создать переменную со словом), 
+//   и выводить в консоль количество гласных и согласных букв в этом слове. 
+//   Ответ должен выводиться шаблонным литералом вида word contains x vowels and y consonants
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+let countVowels = '';
+let countConsonants = '';
+
+const checkWord = function (word) {
+    arrWord = word.toLowerCase().split('');
+    for (let i = 0; i < arrWord.length; i++) {
+        if (vowels.includes(arrWord[i])){
+            countVowels++;
+        } 
+        if (consonants.includes(arrWord[i])){
+            countConsonants++;
+        } 
+    }
+    console.log(`Word ${word} contains ${countVowels} vovels and ${countConsonants} consonants`);
+}
+
+checkWord('JavaScript');
