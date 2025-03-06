c31d0118f4b4e9eeed98c00652646d93a056fdc7
dubinchuk <dubinchuk.e@gmail.com>
2025-03-06 17:39:05 +0300

    task 3 added

--- WebdriverIO/HW_19/src/ui/pages/base.page.ts
+++ WebdriverIO/HW_19/src/ui/pages/base.page.ts
@@ -1,32 +1,42 @@
+import { ElementOrSelector } from '../../data/types/selector.types.js';
+
 const TIMEOUT_5_SECS = 5000;
 
+function isSelectorString(elementOrSelector: ElementOrSelector): elementOrSelector is string {
+  return typeof elementOrSelector === 'string';
+}
+
 export abstract class BasePage {
-  private async findElement(locator: string) {
-    return await $(locator);
+  protected async findElement(locator: ElementOrSelector) {
+    return isSelectorString(locator) ? await $(locator) : locator;
   }
 
-  protected async waitForElement(locator: string, timeout = TIMEOUT_5_SECS, reverse = false) {
+  protected async waitForElement(locator: ElementOrSelector, timeout = TIMEOUT_5_SECS, reverse = false) {
     const element = await this.findElement(locator);
     await element.waitForDisplayed({ timeout, reverse });
     return element;
   }
 
-  protected async click(locator: string, timeout = TIMEOUT_5_SECS) {
+  protected async click(locator: ElementOrSelector, timeout = TIMEOUT_5_SECS) {
     const element = await this.waitForElement(locator, timeout);
     await element.click();
   }
 
-  protected async setValue(locator: string, value: string | number, timeout = TIMEOUT_5_SECS) {
+  protected async setValue(locator: ElementOrSelector, value: string | number, timeout = TIMEOUT_5_SECS) {
     const element = await this.waitForElement(locator, timeout);
     await element.setValue(value);
   }
 
-  protected async getText(locator: string, timeout = TIMEOUT_5_SECS) {
+  protected async getText(locator: ElementOrSelector, timeout = TIMEOUT_5_SECS) {
     const element = await this.waitForElement(locator, timeout);
     return await element.getText();
   }
 
-  protected async selectDropdownValue(dropdownLocator: string, value: string | number, timeout = TIMEOUT_5_SECS) {
+  protected async selectDropdownValue(
+    dropdownLocator: ElementOrSelector,
+    value: string | number,
+    timeout = TIMEOUT_5_SECS
+  ) {
     const element = await this.waitForElement(dropdownLocator, timeout);
     await element.selectByVisibleText(value);
   }
